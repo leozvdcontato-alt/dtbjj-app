f6a62b4c5c5d0ce2f67bd3b79cfef22074a8bf82
leozvdcontato-alt <leozvd.contato@gmail.com>
2026-07-23 07:23:49 +0000

    feat: configura PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,55 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      devOptions: {
+        enabled: true,
+      },
+
+      includeAssets: ["favicon.ico"],
+
+      manifest: {
+        id: "/",
+        name: "Tatame Pro",
+        short_name: "Tatame",
+        description: "Sistema de gestão para academias de Jiu-Jitsu",
+        theme_color: "#991b1b",
+        background_color: "#000000",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
 
   resolve: {
